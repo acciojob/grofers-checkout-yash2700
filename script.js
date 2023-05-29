ec416049024eac08bc54329b191ae842057efb2e
BOGGALA YASWANTH REDDY <93768814+yash2700@users.noreply.github.com>
2023-05-29 20:14:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,47 @@
-const getSumBtn = document.createElement("button");
-getSumBtn.append("Get Total Price");
-document.body.appendChild(getSumBtn);
+const table=document.getElementById("grocery-list")
+const gbtn=document.getElementById("generate-bill")
+// const abtn=document.getElementById("add-items")
+const tbody=document.getElementById("tbody")
+const grandTotal=document.createElement("tr")
+grandTotal.innerHTML=""
+const prices=document.querySelectorAll(".price")
 
-const getSum = () => {
-//Add your code here
-  
-};
+ 
+let sum=0
 
-getSumBtn.addEventListener("click", getSum);
+// function addItems(){
+     
+//     const newItems=document.createElement("tr");
+//     const inputItem=document.createElement("input")
+//     const item=`${inputItem.value}`;
+//     const inputPrice=document.createElement("input")
+//     const price=`${inputPrice.value}`;
+// newItems.innerHTML=`<td>${item}</td>
 
+// <td class="price" data-ns-test="prices">${price}</td>`
+ 
+
+// tbody.appendChild(newItems)
+// table.appendChild(tbody);
+// }
+
+function generateBill(){
+    for(let i=0;i<prices.length;i++){
+        
+        sum+=Number(prices[i].innerText)
+         }
+        
+         
+         grandTotal.innerHTML=`<td contenteditable="false">Total Price</td>
+        <td contenteditable="false"  data-ns-test=grandTotal>${sum}</td>`
+         
+         sum=0;
+        
+         tbody.appendChild(grandTotal)
+
+         
+
+}
+
+
+ gbtn.addEventListener("click",generateBill)
